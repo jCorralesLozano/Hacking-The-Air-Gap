bfab3c83b4831d3eb152f0b56cc13a484b749447
Danny Tran <danny.qa.tran@gmail.com>
2019-10-27 23:24:24 -0700

    Use RtArdino library to output sound to speaker

---
+++ sound_generator/rtaudio_test.cpp
@@ -0,0 +1,68 @@
+#include "RtAudio.h"
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+
+// Two-channel sawtooth wave generator.
+int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+         double streamTime, RtAudioStreamStatus status, void *userData )
+{
+  unsigned int i, j;
+  double *buffer = (double *) outputBuffer;
+  double *lastValues = (double *) userData;
+  if ( status )
+    std::cout << "Stream underflow detected!" << std::endl;
+  // Write interleaved audio data.
+  for ( i=0; i<nBufferFrames; i++ ) {
+    for ( j=0; j<2; j++ ) {
+      *buffer++ = lastValues[j];
+      lastValues[j] += 0.005 * (j+1+(j*0.1));
+      if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
+    }
+  }
+  return 0;
+}
+
+
+int main()
+{
+	RtAudio dac;
+
+	// Check for an audio device periodically until one is found
+	while (dac.getDeviceCount() < 1)
+		usleep(10000000);
+
+	RtAudio::StreamParameters parameters;
+	parameters.deviceId = dac.getDefaultOutputDevice();
+	parameters.nChannels = 2;		// TODO: Stereo, not sure how this affects it though
+	parameters.firstChannel = 0;	// TODO: Not sure what this is for
+
+	unsigned int sampleRate = 44100;
+	unsigned int bufferFrames = 256; // 256 sample frames
+	double data[2];
+	try {
+		dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64,
+						sampleRate, &bufferFrames, &saw, (void *)&data );
+		dac.startStream();
+	}
+	catch ( RtAudioError& e ) {
+		e.printMessage();
+		exit( 0 );
+	}
+
+	char input;
+	std::cout << "\nPlaying ... press <enter> to quit.\n";
+	std::cin.get( input );
+	try {
+		// Stop the stream
+		dac.stopStream();
+	}
+	catch (RtAudioError& e) {
+		e.printMessage();
+	}
+
+	if (dac.isStreamOpen())
+		dac.closeStream();
+
+	return 0;
+}

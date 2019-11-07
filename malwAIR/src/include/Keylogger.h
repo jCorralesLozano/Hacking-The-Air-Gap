4279d0439b9f6de6cf5cdbf7e8382592a13575ff
Danny Tran <danny.qa.tran@gmail.com>
2019-11-06 22:47:09 -0800

    Simple script to read a file in binary mode, deconstruct each byte read down into bits, and play a tone for each bit (different tone for bit 0 and bit 1)

--- malwAIR/src/include/Keylogger.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEYLOGGER_H
-#define KEYLOGGER_H
-
-class Keylogger
-
-#endif // KEYLOGGER_H
--- malwAIR/src/main.cpp
+++ malwAIR/src/main.cpp
@@ -1,30 +1,44 @@
 #include "include/Audio.h"
+#include <fstream>
 #include <iostream>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char** argv) {
+
+	// // Section for debugging.
+	// audio_hack.play_tone(0);
+	// char input;
+	// std::cout << "\nPress <enter> to quit.\n";
+	// std::cin.get(input);
+	// audio_hack.stop_tone();
+
+
+	// If no file is given as input argument, exit
+	if (argc < 2) {
+		std::cout << "No file argument." << std::endl;
+		return 0;
+	}
 	
 	Audio audio_hack;
+	std::ifstream in_file;
 
-	audio_hack.play_tone(0);
-	char input;
-	std::cout << "\nPlaying 0 press <enter> to quit.\n";
-	std::cin.get(input);
-	audio_hack.stop_tone();
+	// Open file to read in binary mode
+	in_file.open(argv[1], std::ifstream::in | std::ifstream::binary);
+	char c;
 
-	audio_hack.play_tone(1);
-	std::cout << "\nPlaying 1 press <enter> to quit.\n";
-	std::cin.get(input);
-	audio_hack.stop_tone();
+	// Smallest reading size is 1 byte at a time
+	while (in_file.get(c)) {
+		// Deconstruct the byte read into bits
+		for (int i = 7; i >= 0; --i) {
+			// Output a tone for each bit with a .5 second pause in-between each
+			// std::cout << ((c >> i) & 1);
+			audio_hack.play_tone((c >> i) & 1);
+			usleep(500000);
+			audio_hack.stop_tone();
+		}
+		// std::cout << std::endl;
+	}
 
-	audio_hack.play_tone(0);
-	std::cout << "\nPlaying 0 press <enter> to quit.\n";
-	std::cin.get(input);
-	audio_hack.stop_tone();
-
-	audio_hack.play_tone(1);
-	std::cout << "\nPlaying 1 press <enter> to quit.\n";
-	std::cin.get(input);
-	audio_hack.stop_tone();
-
+	in_file.close();
 	return 0;
 }

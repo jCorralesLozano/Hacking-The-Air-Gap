4315cc915082337265470cc62f73f255d67e390d
jCorralesLozano <jesselozano17@gmail.com>
2019-11-08 13:06:00 -0800

    Can log correct frequencies near the target frequencies

--- audio input/record_realtime_visualization.py
+++ audio input/record_realtime_visualization.py
@@ -1,16 +1,16 @@
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
+from scipy.fftpack import fft, fftfreq
 import time
 import csv
 
 CHUNK = 4096  # number of data points to read at a time
 RATE = 44100  # time resolution of the recording device (Hz)
-FREQUENCY = 4000  # target frequency
-BANDWIDTH = 100  # boundary from the target frequency
-LOWER = FREQUENCY - BANDWIDTH  # lower frequency bound
-UPPER = FREQUENCY + BANDWIDTH  # upper frequency bound
+LOWER = 440  # lower frequency bound
+UPPER = 6000  # upper frequency bound
+BANDWIDTH = 50  # boundary from the target frequency
+FRATE = 44100  # frame rate
 
 p = pyaudio.PyAudio()  # start the PyAudio class
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
@@ -22,7 +22,7 @@ csvfile = open('frequency.csv', 'w')
 # create the file writer object
 filewriter = csv.writer(csvfile, delimiter=',',
                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-filewriter.writerow(['Peak Amplitude', 'Frequency (Hz)', 'Time (s)'])
+filewriter.writerow(['Frequency (Hz)', 'Time (s)'])
 
 # start the timer
 startTime = time.time()
@@ -33,28 +33,36 @@ timeXAxis = []
 
 # create a numpy array holding a single read of audio data
 for i in range(100):  # to it a few times just to see
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
     data = fft(data)  # compute the fft (complex numbers list)
-    d = len(data) / 2  # you only need half of the fft list (real signal symmetry)
+    # print out the frequencies found
+    freqs = fftfreq(len(data))
+    # print(freqs.min(), freqs.max())
 
+    # Find the peak in the coefficients
+    idx = np.argmax(np.abs(data))
+    freq = freqs[idx]
+    freq_in_hertz = abs(freq * FRATE)
+    print(freq_in_hertz)
     # end timer
     endTime = time.time()
 
-    # freq = index
-    # magnitude = value
-    peak = np.max(abs(data))
-    for freq, magnitude in enumerate(data):
-        mag = abs(magnitude)
-        if peak == mag and LOWER <= freq <= UPPER:
-            print(f"Peak Amplitude: {peak}, Magnitude: {abs(magnitude)}, freq: {freq}")
-            filewriter.writerow([peak, freq, endTime - startTime])
-            freqYAxis.append(freq)
-            timeXAxis.append(endTime - startTime)
-        elif peak == mag:
-            print(f"peak: {peak}, freq: {freq}")
-            freqYAxis.append(freq)
-            timeXAxis.append(endTime - startTime)
-            filewriter.writerow([peak, freq, endTime - startTime])
+    # check if the frequency is near the UPPER or LOWER boundary
+    if UPPER - BANDWIDTH <= freq_in_hertz <= UPPER + BANDWIDTH:
+        filewriter.writerow([freq, endTime - startTime])
+        freqYAxis.append(freq_in_hertz)
+        timeXAxis.append(endTime - startTime)
+    elif LOWER - BANDWIDTH <= freq_in_hertz <= LOWER + BANDWIDTH:
+        freqYAxis.append(freq_in_hertz)
+        timeXAxis.append(endTime - startTime)
+        filewriter.writerow([freq, endTime - startTime])
+    else:
+        freqYAxis.append(0)  # record 0 Hz frequency
+        timeXAxis.append(endTime - startTime)
+        filewriter.writerow([freq, endTime - startTime])
+
+# close the csv file
+csvfile.close()
 
 # plot frequency vs time
 plt.plot(timeXAxis, freqYAxis, 'r')
@@ -64,9 +72,6 @@ plt.savefig('freq_v_time.png')  # savefig must be called before show
                                 # because show() erases the plot afterwards
 plt.show()
 plt.close()
-
-# close the csv file
-csvfile.close()
 
 # close the stream gracefully
 stream.stop_stream()
--- audio input/wav_freq_fft.py
+++ audio input/wav_freq_fft.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
+from scipy.fftpack import fft, fftfreq
 from scipy.io import wavfile # get the api
-fs, data = wavfile.read('200.wav') # load the data
+import numpy as np
+
+fs, data = wavfile.read('1000.wav') # load the data
 a = data.T[0] # this is a two channel soundtrack, I get the first track
 b=[ele for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
 c = fft(b) # calculate fourier transform (complex numbers list)
 d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+
+# print out the frequencies found
+freqs = fftfreq(len(c))
+print(freqs.min(), freqs.max())
+
+# Find the peak in the coefficients
+frate = 44100
+idx = np.argmax(np.abs(c))
+freq = freqs[idx]
+freq_in_hertz = abs(freq*frate)
+print(freq_in_hertz)
+
 plt.plot(abs(c[:round(d-1)]),'r') 
 plt.show()

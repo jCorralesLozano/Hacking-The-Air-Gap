427f014ce83763a22b01c5d541ff260d45c77d8f
syamHaque <msyamhaque@gmail.com>
2020-02-24 02:23:59 -0800

    got pi outputting any frequencies at sine wave

--- audio output/audio_output.py
+++ audio output/audio_output.py
@@ -1,82 +1,39 @@
-#EECS 159A
+#EECS 159
 #Mohammed Haque
 #11/21/19
-''' pg_midi_sound101.py
-play midi music files (also mp3 files) using pygame
-tested with Python273/331 and pygame192 by vegaseat
-'''
-#code modified from here: https://www.daniweb.com/programming/software-development/code/454835/let-pygame-play-your-midi-or-mp3-files
-
-
 #!/usr/bin/python
 
 import sys
 import pygame as pg
+import numpy as np
+import pygame.sndarray
 import os
 import time
 
-
-def play_music(music_file):
-    '''
-    stream music with mixer.music module in blocking manner
-    this will stream the sound from disk while playing
-    '''
-    clock = pg.time.Clock()
-    try:
-        pg.mixer.music.load(music_file)
-        print("Music file {} loaded!".format(music_file))
-    except pygame.error:
-        print("File {} not found! {}".format(music_file, pg.get_error()))
-        return
-
-    pg.mixer.music.play()
-    
-    # for x in range(0,100):
-    #     pg.mixer.music.set_volume(float(x)/100.0)
-    #     time.sleep(.0075)
-    # # check if playback has finished
-    while pg.mixer.music.get_busy():
-        clock.tick(30)
-
-
 freq = 44100    # audio CD quality
 bitsize = -16   # unsigned 16 bit
-channels = 2    # 1 is mono, 2 is stereo
-buffer = 2048   # number of samples (experiment to get right sound)
-pg.mixer.init(freq, bitsize, channels, buffer)
-# optional volume 0 to 1.0
-pg.mixer.music.set_volume(1.0)
-
-
-##if len(sys.argv) > 1:
-##
-##    try: 
-##        user_volume = float(sys.argv[1])
-##    except ValueError:
-##        print ("Volume argument invalid. Please use a float (0.0 - 1.0)")
-##        pg.mixer.music.fadeout(1000)
-##        pg.mixer.music.stop()
-##        raise SystemExit
-##
-##    print("Playing at volume: " + str(user_volume)+ "\n")
-##    pg.mixer.music.set_volume(user_volume)
-mp3s = []
-for file in os.listdir("."):
-    if file.endswith(".wav"):
-        mp3s.append(file)
-
-print (mp3s)
+channels = 1    # 1 is mono, 2 is stereo
+buffer = 4096   # number of samples (experiment to get right sound)
+# 44.1kHz, 16-bit signed, mono
+pg.mixer.pre_init(freq, bitsize, channels) 
+pg.init()
+# buffer : the peak ; volume ; loudness
+# 440 : the frequency in hz
+# if astype int16 not specified sound will get very noisy, because we have defined it as 16 bit mixer at mixer.pre_init()
+# ( probably due to int overflow resulting in non continuous sound data)
+arr = np.array([buffer * np.sin(2.0 * np.pi * 5000 * x / freq) for x in range(0, freq)]).astype(np.int16)
+sound = pg.sndarray.make_sound(arr)
 
 try:
     while True:
-        for x in mp3s: 
-            play_music(x)
-            time.sleep(0)
+        # -1 means loop unlimited times
+        sound.play(-1)
+        # determines length of sound
+        pg.time.delay(1000)
+        sound.stop()
+        time.sleep(0)
 except KeyboardInterrupt:
     # if user hits Ctrl/C then exit
     # (works only in console mode)
-    pg.mixer.music.fadeout(1000)
-    pg.mixer.music.stop()
+    sound.stop()
     raise SystemExit
-##else:
-##    print("Please specify volume as a float! (0.0 - 1.0)")
